e760d6ac2c2c1662426d8aa07a9e42c4eea81e8c
Sharrief Shabazz <me@sharrief.com>
2023-12-19 00:27:09 +0000

    Add en-us messages

---
+++ src/util/messages.en.ts
@@ -0,0 +1,6 @@
+const messages = {
+  prompt: "Enter your email address, GitHub repo, then click Submit.",
+  invalidEmail: "The email appears to be invalid. Please double check the address.",
+  invalidGitHubURL: "The GitHub repository URL appears to be invalid. Please double check the URL."
+}
+export default messages;
